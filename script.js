ee2b56bacf6be9b7603bdb1bea120a50e675412f
Brian-Mutua12 <86000295+Brian-Mutua12@users.noreply.github.com>
2023-02-16 21:23:25 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+var typed = new Typed(".multiple-text", {
+    strings: ["Cyber Security Specialist", "Web Developer", "Software Engineer"],
+    typeSpeed: 100, backSpeed: 100, backDelay: 1000, loop: true
+})
